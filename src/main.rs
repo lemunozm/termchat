66dcf65583af6e222ff880fa0d895f4410a2c6fe
lemunozm <lemunozm@gmail.com>
2020-09-06 17:48:29 +0200

    Added CLI

--- src/main.rs
+++ src/main.rs
@@ -6,15 +6,34 @@ mod util;
 
 use application::{Application};
 
-fn main() {
-    let addr = "239.255.0.1:3010".parse().unwrap();
+use clap::{App, Arg};
 
-    let args: Vec<String> = std::env::args().collect();
-    let name = match args.get(1) {
-        Some(name) => name.into(),
-        None => whoami::username(),
+fn main() {
+    let os_username = whoami::username();
+
+    let matches = App::new(clap::crate_name!())
+        .version(clap::crate_version!())
+        .author(clap::crate_authors!())
+        .about(clap::crate_description!())
+        .arg(Arg::with_name("discovery")
+            .long("discovery")
+            .short("d")
+            .default_value("238.255.0.1:5877")
+            .help("Multicast address to found others 'termchat' applications"))
+        .arg(Arg::with_name("username")
+            .long("username")
+            .short("u")
+            .default_value(&os_username)
+            .help("Name used as user idenfication"))
+        .get_matches();
+
+    let addr = match matches.value_of("discovery").unwrap().parse() {
+        Ok(addr) => addr,
+        Err(_) => return eprintln!("'discovery' must be a valid multicast address"),
     };
-    println!("{}", name);
+
+    let name = matches.value_of("username").unwrap();
+
     if let Ok(mut app) = Application::new(addr, &name) {
         app.run()
     }
