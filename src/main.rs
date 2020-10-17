81ce882c3475ec264c66d8921fed76d583211828
Nbiba Bedis <bedisnbiba@gmail.com>
2020-10-17 12:11:49 +0100

    cargo fmt

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
 mod application;
 mod state;
-mod ui;
 mod terminal_events;
+mod ui;
 mod util;
 
-use application::{Application};
+use application::Application;
 
 use clap::{App, Arg};
 
@@ -15,16 +15,20 @@ fn main() {
         .version(clap::crate_version!())
         .author(clap::crate_authors!())
         .about(clap::crate_description!())
-        .arg(Arg::with_name("discovery")
-            .long("discovery")
-            .short("d")
-            .default_value("238.255.0.1:5877")
-            .help("Multicast address to found others 'termchat' applications"))
-        .arg(Arg::with_name("username")
-            .long("username")
-            .short("u")
-            .default_value(&os_username)
-            .help("Name used as user idenfication"))
+        .arg(
+            Arg::with_name("discovery")
+                .long("discovery")
+                .short("d")
+                .default_value("238.255.0.1:5877")
+                .help("Multicast address to found others 'termchat' applications"),
+        )
+        .arg(
+            Arg::with_name("username")
+                .long("username")
+                .short("u")
+                .default_value(&os_username)
+                .help("Name used as user idenfication"),
+        )
         .get_matches();
 
     let addr = match matches.value_of("discovery").unwrap().parse() {
--- src/util.rs
+++ src/util.rs
@@ -4,7 +4,8 @@ pub trait SplitEach {
 
 impl SplitEach for str {
     fn split_each(&self, n: usize) -> Vec<&str> {
-        let mut splitted = Vec::with_capacity(self.len() / n + if self.len() % n > 0 {1} else {0});
+        let mut splitted =
+            Vec::with_capacity(self.len() / n + if self.len() % n > 0 { 1 } else { 0 });
         let mut last = self;
         while !last.is_empty() {
             let (chunk, rest) = last.split_at(std::cmp::min(n, last.len()));
